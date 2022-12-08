b1e2cedc1ce29c7999b8f55c7b94d70760e578ce
Fawato <109968429+Fawato@users.noreply.github.com>
2022-12-08 18:38:03 +0530

    Update JavaEcosystemCapabilitiesExtension.java

--- src/main/java/org/gradlex/javaecosystem/capabilities/JavaEcosystemCapabilitiesExtension.java
+++ src/main/java/org/gradlex/javaecosystem/capabilities/JavaEcosystemCapabilitiesExtension.java
@@ -32,5 +32,79 @@ public abstract class JavaEcosystemCapabilitiesExtension {
         return allCapabilities;
     }
 
+    package org.gradlex.javaecosystem.capabilities;
+
+import org.gradle.api.provider.SetProperty;
+
+import java.util.Set;
+
+public abstract class JavaEcosystemCapabilitiesExtension {
+
+    private final Set<String> allCapabilities;
+
+    public JavaEcosystemCapabilitiesExtension(Set<String> allCapabilities) {
+        this.allCapabilities = allCapabilities;
+    }
+
+    public Set<String> getAllCapabilities() {
+        return allCapabilities;
+    }
+
+    public abstract SetProperty<String> getDeactivatedResolutionStrategies();
+    package org.gradlex.javaecosystem.capabilities;
+
+import org.gradle.api.provider.SetProperty;
+
+import java.util.Set;
+
+public abstract class JavaEcosystemCapabilitiesExtension {
+
+    private final Set<String> allCapabilities;
+
+    public JavaEcosystemCapabilitiesExtension(Set<String> allCapabilities) {
+        this.allCapabilities = allCapabilities;
+    }
+
+    public Set<String> getAllCapabilities() {
+        return allCapabilities;
+    }
+
+    public abstract SetProperty<String> getDeactivatedResolutionStrategies();package org.gradlex.javaecosystem.capabilities;
+
+import org.gradle.api.provider.SetProperty;
+
+import java.util.Set;
+
+public abstract class JavaEcosystemCapabilitiesExtension {
+
+    private final Set<String> allCapabilities;
+
+    public JavaEcosystemCapabilitiesExtension(Set<String> allCapabilities) {
+        this.allCapabilities = allCapabilities;
+    }
+
+    public Set<String> getAllCapabilities() {
+        return allCapabilities;
+    }
+
+    public abstract SetProperty<String> getDeactivatedResolutionStrategies();package org.gradlex.javaecosystem.capabilities;
+
+import org.gradle.api.provider.SetProperty;
+
+import java.util.Set;
+
+public abstract class JavaEcosystemCapabilitiesExtension {
+
+    private final Set<String> allCapabilities;
+
+    public JavaEcosystemCapabilitiesExtension(Set<String> allCapabilities) {
+        this.allCapabilities = allCapabilities;
+    }
+
+    public Set<String> getAllCapabilities() {
+        return allCapabilities;
+    }
+
+    public abstract SetProperty<String> getDeactivatedResolutionStrategies();
     public abstract SetProperty<String> getDeactivatedResolutionStrategies();
 }
